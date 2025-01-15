a552c3228bf55209f8069d7141e0d6386b74dafd
AdiX <142919932+AdiInfiniteLoop@users.noreply.github.com>
2025-01-15 17:47:26 +0530

    Create Carousel.tsx

---
+++ Carousel.tsx
@@ -0,0 +1,16 @@
+
+export const Carousel = () => {
+  return (
+    <div className='container'>
+        <div className="container-carousel">
+            <div className="carousel">
+                <div className="carousel-item" style={{backgroundColor:'red'}}>1</div>
+                <div className="carousel-item" style={{backgroundColor:'blue'}}>2</div>
+                <div className="carousel-item" style={{backgroundColor:'yellow'}}>3</div>
+                <div className="carousel-item" style={{backgroundColor:'red'}}>1</div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
